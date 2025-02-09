beb38c7f2faa4b8a888902eec3264a514a01beac
keithga <keithga@keithga.com>
2025-02-09 09:20:05 -0700

    Remove build files

--- Clean-AllBuilds.ps1
+++ Clean-AllBuilds.ps1
@@ -1,4 +1,8 @@
 
-## Untested
-
 wsl.exe -- rm -r -f ~/ipxe/src/bin* 
+
+while ( $true ) { try { stop-transcript } catch { break} }
+if (![string]::IsNullOrEmpty($PSScriptRoot)) {
+    remove-item -Recurse -Force $PSScriptRoot\Build
+    remove-item -Recurse -force $PSScriptRoot\config\*\tmp
+}
