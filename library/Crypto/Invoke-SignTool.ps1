d69ebe1201be6106d72968634d210e320e192a66
keithga <keithga@keithga.com>
2025-02-09 09:15:27 -0700

    Fix SIgntool path

--- library/Crypto/Invoke-SignTool.ps1
+++ library/Crypto/Invoke-SignTool.ps1
@@ -7,26 +7,26 @@ function Invoke-SignTool {
 
     #region Find openssl
     if ( -not ( get-command signtool.exe -ErrorAction SilentlyContinue ) ) { 
-        $SignToolPath = (Get-ItemProperty -Path 'HKLM:\software\Microsoft\Windows Kits').KitsRoot10
-        if ( -not (test-path $SignToolPath\bin\x64\openssl.exe )) { throw "Missing SignTool" }
+        $SignToolPath = 'C:\Program Files (x86)\Windows Kits\10'
+        if ( -not (test-path $SignToolPath\bin\x64\signtool.exe )) { throw "Missing SignTool" }
 
         $env:path += ";$SignToolPath\bin\x64"
 
         ## Other places here
 
-        if ( -not ( get-command openssl.exe ) ) { 
-            throw "openssl.exe not found. Install Git for Windows" 
+        if ( -not ( get-command signtool.exe ) ) { 
+            throw "signtool.exe not found. Install ADK/SDK for Windows" 
         }
     }
     #endregion     
 
-    if ( (get-item Cert:\CurrentUser\my -CodeSigningCert | Measure).Count -ne 1 ) {
+    if ( (get-item Cert:\CurrentUser\my\* -CodeSigningCert | Measure).Count -ne 1 ) {
         throw "Can't find Code signing Cert."
     }
 
     Copy-Item @PSBoundParameters -force
-    write-verbose "Sign /q /FD sha256 /v $Target" 
-    SignTool.exe Sign /q /FD sha256 /v $Target
+    write-verbose "Sign /q /FD sha256 $Dest" 
+    SignTool.exe Sign /q /a /FD sha256 $Dest
 
     ### XXX BUGBUG TODO - Will need to change this for production Signing 
 
